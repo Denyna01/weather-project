12f6f545cc6c56780920fea9beccef3d6a711630
Denisa <denissa.budajova@gmail.com>
2022-10-18 22:21:33 +0200

    actual date

--- js.js
+++ js.js
@@ -1,42 +1,28 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
+let now = new Date();
+let h1 = document.querySelector("h1");
+let date = now.getDate();
+let hours = now.getHours().toString().padStart(2, "0");
+let minutes = now.getMinutes().toString().padStart(2, "0");
+let year = now.getFullYear();
 
-// write your code here
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-let city = prompt("What city?");
-if (weather[city.toLowerCase()]) {
-  let cityObj = weather[city.toLowerCase()];
-  alert(
-    city +
-      "\ntemp = " +
-      cityObj.temp +
-      "°C\nhumidity = " +
-      cityObj.humidity +
-      "%"
-  );
-} else {
-  alert(
-    "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+" +
-      city
-  );
-}
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+let month = months[now.getMonth()];
+
+h1.innerHTML = `${day}, Oct ${date}, ${hours}:${minutes}, ${year} `;
