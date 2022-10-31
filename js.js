a3238843dd2a394cf7edbe6330443c2d200b2c6d
Denisa <denissa.budajova@gmail.com>
2022-10-31 22:38:19 +0100

    added cities & icons

--- js.js
+++ js.js
@@ -4,13 +4,20 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector ("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round (response.data.wind.speed);
+  iconElement.setAttribute(
+    "src", response.data.condition.icon_url)
 }
 
 let apiKey = "acfd102152eb04ebf39439at08oab84c";
+let city = "Sidney";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=acfd102152eb04ebf39439at08oab84c&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
@@ -44,3 +51,12 @@ let months = [
 let month = months[now.getMonth()];
 
 h1.innerHTML = `${day}, Oct ${date}, ${hours}:${minutes}, ${year} `;
+
+let cityName = document.querySelector("#city-search").addEventListener("click", getDataAboutCity)
+
+function getDataAboutCity() {
+  city = document.querySelector("#city-field").value; 
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${encodeURI(city)}&key=acfd102152eb04ebf39439at08oab84c&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayTemperature);
+}
