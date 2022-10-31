028313875a113dc48a85a42902a74b55a12b32fd
Denisa <denissa.budajova@gmail.com>
2022-10-31 23:34:04 +0100

    added conversion C | F

--- js.js
+++ js.js
@@ -5,19 +5,17 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let iconElement = document.querySelector ("#icon");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
-  windElement.innerHTML = Math.round (response.data.wind.speed);
-  iconElement.setAttribute(
-    "src", response.data.condition.icon_url)
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute("src", response.data.condition.icon_url);
 }
 
 let apiKey = "acfd102152eb04ebf39439at08oab84c";
-let city = "Sidney";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=acfd102152eb04ebf39439at08oab84c&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
@@ -52,11 +50,25 @@ let month = months[now.getMonth()];
 
 h1.innerHTML = `${day}, Oct ${date}, ${hours}:${minutes}, ${year} `;
 
-let cityName = document.querySelector("#city-search").addEventListener("click", getDataAboutCity)
+let cityName = document
+  .querySelector("#city-search")
+  .addEventListener("click", getDataAboutCity);
 
 function getDataAboutCity() {
-  city = document.querySelector("#city-field").value; 
-  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${encodeURI(city)}&key=acfd102152eb04ebf39439at08oab84c&units=metric`;
+  city = document.querySelector("#city-field").value;
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${encodeURI(
+    city
+  )}&key=acfd102152eb04ebf39439at08oab84c&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayTemperature);
 }
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (17 * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let fahrenheitLink = document.querySelector("#fahreheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
